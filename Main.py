8327bf014fc2210ee2d8492be4a0856dce24f77b
Anderson2811 <anderson.andres.guerrero@gmail.com>
2014-05-16 04:46:39 -0430

    Subiendo Main.py

---
+++ Main.py
@@ -0,0 +1,4 @@
+print("Suma de dos Numeros")
+n1=int(raw_input("Intoduzca el valor del Primer sumero a sumar"))
+n2=int(raw_input("Intoduzca el valor del Primer segundo a sumar"))
+print("El Resultado es: "+str(n1+n2))
