4cd401e33a16d2c7c34197eca628825e0d7ee30d
Anderson2811 <anderson.andres.guerrero@gmail.com>
2014-05-16 05:12:49 -0430

    Subiendo prueba repetitiva

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
 print("Suma de dos Numeros")
-n1=int(raw_input("Intoduzca el valor del Primer sumero a sumar"))
-n2=int(raw_input("Intoduzca el valor del Primer segundo a sumar"))
-print("El Resultado es: "+str(n1+n2))
+x=0
+while x<1000:
+    n1=int(raw_input("Intoduzca el valor del Primer sumero a sumar: "))
+    n2=int(raw_input("Intoduzca el valor del Primer segundo a sumar: "))
+    print("El Resultado es: "+str(n1+n2))
+    rsp=raw_input("Quiere sumar de nuevo?  S/N :")
+    if rsp=="n" or rsp=="N":
+       break
