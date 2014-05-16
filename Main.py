67a453af4fdda70e219c8c9aaaee699eb88cbac5
Anderson2811 <anderson.andres.guerrero@gmail.com>
2014-05-16 13:17:58 -0430

    modificacion

--- Main.py
+++ Main.py
@@ -1,9 +1,21 @@
-print("Suma de dos Numeros")
 x=0
+resul=0
 while x<1000:
-    n1=int(raw_input("Intoduzca el valor del Primer sumero a sumar: "))
-    n2=int(raw_input("Intoduzca el valor del Primer segundo a sumar: "))
-    print("El Resultado es: "+str(n1+n2))
-    rsp=raw_input("Quiere sumar de nuevo?  S/N :")
+    n1=int(raw_input("Intoduzca el Primer valor : "))
+    n2=int(raw_input("Intoduzca el segundo valor : "))
+    tipo=raw_input("Que desea Hacer (+)(-)(*)(/) ")
+    if tipo=="+":
+       resul=n1+n2
+    elif tipo=="-":
+       resul=n1-n2   
+    elif tipo=="*":
+       resul=n1*n2
+    elif tipo=="/":
+       resul=n1/n2
+
+
+    print("El Resultado es: "+str(resul))
+    rsp=raw_input("Quieres Realizar Otra Operacion?  S/N :")
+    
     if rsp=="n" or rsp=="N":
        break
